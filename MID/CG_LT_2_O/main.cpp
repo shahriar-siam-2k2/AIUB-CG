2ca737beadef03c8bb79948e6a5fa6386dcef194
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2025-11-03 02:10:33 +0600

    CG_LT_2_O_Finished

--- MID/CG_LT_2_O/main.cpp
+++ MID/CG_LT_2_O/main.cpp
@@ -17,12 +17,21 @@ void display() {
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    gluOrtho2D(-3.0, 3.0, -3.0, 3.0);
+
     for(int i=0; i<4; i++) {
 
         for(int j=0; j<4; j++) {
 
+            glPushMatrix();
+
             float x = START_POINT + (j * SQ_SIZE);
             float y = START_POINT + (i * SQ_SIZE);
+
+            glTranslatef(x, y, 0.0f);
 
             if((i + j) % 2 == 0) {
                 glColor3f(1.0, 1.0, 1.0);
@@ -46,8 +55,9 @@ int main(int argc, char** argv) {
 
     glutInit(&argc, argv);
     glutCreateWindow("4X4 Chess Board");
-    glutInitWindowSize(320, 320);
+    glutInitWindowSize(500, 500);
     glutDisplayFunc(display);
+    glutMainLoop();
 
     return 0;
 }
