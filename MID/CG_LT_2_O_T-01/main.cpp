b3ac9cb7344af1e649c411308d5f124529030b92
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2025-11-15 12:36:28 +0600

    Pushed to Desktop

--- MID/CG_LT_2_O_T-01/main.cpp
+++ MID/CG_LT_2_O_T-01/main.cpp
@@ -42,13 +42,13 @@ void vase(float x, float y, float s) {
         glVertex2f(x-0.08f*s, y-0.24f*s); //A9
         glVertex2f(x-0.04f*s, y-0.25f*s); //B9
         glVertex2f(x-0.00f*s, y-0.25f*s); //F9
-        
+
         glVertex2f(x-0.00f*s, y-0.25f*s); //F9
         glVertex2f(x+0.04f*s, y-0.25f*s); //C9
         glVertex2f(x+0.08f*s, y-0.24f*s); //D9
         glVertex2f(x+0.15f*s, y-0.20f*s); //N
     glEnd();
-    
+
     //vase - Lower Part
 
     glColor3f(0.74118f, 0.33725f, 0.89804f);
@@ -82,6 +82,8 @@ void vase(float x, float y, float s) {
 
     //trunk
 
+    // Main Trunk (Mid)
+
     glColor3f(0.18431f, 0.12941f, 0.60392f);
 
     glBegin(GL_QUADS);
@@ -90,6 +92,106 @@ void vase(float x, float y, float s) {
         glVertex2f(x-0.01f*s, y+0.28f*s); //U
         glVertex2f(x+0.01f*s, y+0.28f*s); //V
         glVertex2f(x+0.01f*s, y+0.00f*s); //W
+
+    glEnd();
+
+    // Left Trunk-1
+
+    glColor3f(0.18431f, 0.12941f, 0.60392f);
+
+    glBegin(GL_QUADS);
+
+        glVertex2f(x-0.05f*s, y+0.03f*s); //G1
+        glVertex2f(x-0.03f*s, y+0.00f*s); //Z
+        glVertex2f(x-0.04f*s, y+0.06f*s); //A1
+        glVertex2f(x-0.06f*s, y+0.06f*s); //F1
+
+        glVertex2f(x-0.06f*s, y+0.06f*s); //F1
+        glVertex2f(x-0.04f*s, y+0.06f*s); //A1
+        glVertex2f(x-0.08f*s, y+0.14f*s); //B1
+        glVertex2f(x-0.10f*s, y+0.13f*s); //E1
+
+        glVertex2f(x-0.10f*s, y+0.13f*s); //E1
+        glVertex2f(x-0.08f*s, y+0.14f*s); //B1
+        glVertex2f(x-0.14f*s, y+0.21f*s); //C1
+        glVertex2f(x-0.15f*s, y+0.20f*s); //D1
+
+    glEnd();
+
+    // Left Trunk-2
+
+    glColor3f(0.18431f, 0.12941f, 0.60392f);
+
+    glBegin(GL_QUADS);
+
+        glVertex2f(x-0.03f*s, y+0.00f*s); //Z
+        glVertex2f(x-0.04f*s, y+0.00f*s); //O1
+        glVertex2f(x-0.05f*s, y+0.02f*s); //N1
+        glVertex2f(x-0.05f*s, y+0.03f*s); //G1
+
+        glVertex2f(x-0.05f*s, y+0.03f*s); //G1
+        glVertex2f(x-0.05f*s, y+0.02f*s); //N1
+        glVertex2f(x-0.10f*s, y+0.07f*s); //M1
+        glVertex2f(x-0.10f*s, y+0.08f*s); //H1
+
+        glVertex2f(x-0.10f*s, y+0.08f*s); //H1
+        glVertex2f(x-0.10f*s, y+0.07f*s); //M1
+        glVertex2f(x-0.16f*s, y+0.10f*s); //L1
+        glVertex2f(x-0.16f*s, y+0.11f*s); //I1
+
+        glVertex2f(x-0.16f*s, y+0.11f*s); //I1
+        glVertex2f(x-0.16f*s, y+0.10f*s); //L1
+        glVertex2f(x-0.21f*s, y+0.11f*s); //K1
+        glVertex2f(x-0.21f*s, y+0.12f*s); //J1
+
+    glEnd();
+
+    // Right Trunk-1
+
+    glColor3f(0.18431f, 0.12941f, 0.60392f);
+
+    glBegin(GL_QUADS);
+
+        glVertex2f(x-0.03f*s, y+0.00f*s); //Z
+
+    glEnd();
+
+
+    // Leaves
+
+    // Leaf - Trunk-1 - Left
+
+    glColor3f(0.00392f, 0.74902f, 0.60000f);
+
+    glBegin(GL_QUADS);
+
+        glVertex2f(x-0.01f*s, y+0.13f*s); //T3
+        glVertex2f(x-0.01f*s, y+0.19f*s); //B4
+        glVertex2f(x-0.04f*s, y+0.24f*s); //A4
+        glVertex2f(x-0.08f*s, y+0.18f*s); //U3
+
+        glVertex2f(x-0.08f*s, y+0.18f*s); //U3
+        glVertex2f(x-0.10f*s, y+0.21f*s); //V3
+        glVertex2f(x-0.10f*s, y+0.27f*s); //W3
+        glVertex2f(x-0.04f*s, y+0.24f*s); //A4
+
+    glEnd();
+
+    // Leaf - Trunk-1 - Right
+
+    glColor3f(0.00392f, 0.74902f, 0.60000f);
+
+    glBegin(GL_QUADS);
+
+        glVertex2f(x+0.01f*s, y+0.19f*s); //C4
+        glVertex2f(x+0.01f*s, y+0.10f*s); //J4
+        glVertex2f(x+0.08f*s, y+0.17f*s); //I4
+        glVertex2f(x+0.09f*s, y+0.20f*s); //H4
+
+        glVertex2f(x+0.09f*s, y+0.20f*s); //H4
+        glVertex2f(x+0.08f*s, y+0.27f*s); //F4
+        glVertex2f(x+0.04f*s, y+0.24f*s); //E4
+        glVertex2f(x+0.01f*s, y+0.19f*s); //C4
 
     glEnd();
 
@@ -111,7 +213,7 @@ void display() {
 int main(int argc, char** argv) {
 
     glutInit(&argc, argv);
-    glutInitWindowSize(700, 700);
+    glutInitWindowSize(600, 600);
     glutCreateWindow("CG_LT_2_O_T-01 | Vase");
     glutDisplayFunc(display);
     glutMainLoop();
